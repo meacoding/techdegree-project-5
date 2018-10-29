cd8bf31bf22017ffdf1878b3612055cdd6564c8d
meacoding <16675876+meacoding@users.noreply.github.com>
2018-10-29 16:59:30 -0400

    Tweaked styling and adding img thumbnails

--- _js/app.js
+++ _js/app.js
@@ -21,6 +21,7 @@ $(document).ready(function() {
     //Loop through image captions in Lighthouse attr.
     for (let i = 0; i < $images.length; i++) {
       let $caption = $($images[i]).attr("data-title");
+
       //If matched, filter out images
       if ($caption.toUpperCase().indexOf($searchInput) > -1) {
         $($images[i]).css({ display: "" });
@@ -29,6 +30,35 @@ $(document).ready(function() {
       }
     }
   }
-
+  //Event listener
   $("#search").keyup(filterImages);
+
+
+// Keep for future reference if DOM doesn't load full size images through href.
+
+//   const imageArray = [
+//     'photos/01.jpg',
+//     'photos/02.jpg',
+//     'photos/03.jpg',
+//     'photos/04.jpg',
+//     'photos/05.jpg',
+//     'photos/06.jpg',
+//     'photos/07.jpg',
+//     'photos/08.jpg',
+//     'photos/09.jpg',
+//     'photos/10.jpg',
+//     'photos/11.jpg',
+//     'photos/12.jpg'
+//   ];
+
+//   function preload(arrayOfImages) {
+//     $(arrayOfImages).each(function(){
+//         $('<img/>')[0].src = this;
+//     });
+// }
+
+//   // Usage:
+
+//   preload(imageArray);
+
 });
